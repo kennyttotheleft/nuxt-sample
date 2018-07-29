2a2d31f98c40a977f1c579bb4a53b0ac7f41f95d
TANAKAORIGINAL <tanakaoriginal@gmail.com>
2018-07-29 12:47:25 +0900

    Add config for middleware

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,25 @@ module.exports = {
         })
       }
     }
-  }
+  },
+  /*
+  ** Add server middleware
+  ** Nuxt.js uses `connect` module as server
+  ** So most of express middleware works with nuxt.js server middleware
+  */
+  serverMiddleware: [
+    // body-parser middleware
+    bodyParser.json(),
+    // session middleware
+    session({
+      secret: 'super-secret-key',
+      resave: false,
+      saveUninitialized: false,
+      cookie: { maxAge: 60000 }
+    }),
+    // Api middleware
+    // We add /api/login & /api/logout routes
+    '~/api'
+  ]
 }
 
